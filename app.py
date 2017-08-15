7143c3f5825a0bc45a0f782773f8292b84e31e4f
Phil Evans <phil.evans@gallagher.com>
2017-08-16 09:20:23 +1200

    PUT and POST methods

--- app.py
+++ app.py
@@ -21,23 +21,15 @@ def state_of_city(city):
         raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
             city, ', '.join(CITIES_TO_STATE.keys())))
 
+@app.route('/resource/{value}', methods=['PUT'])
+def put_test(value):
+    return {"value": value}
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
