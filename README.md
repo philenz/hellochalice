15bcac3527816cf0eb5b407fb30ffd71241b2b96
Phil Evans <phil.evans@gallagher.com>
2017-08-15 15:59:00 +1200

    Add a new route

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ### Chalice Quickstart
-
 * http://chalice.readthedocs.io/en/latest/quickstart.html
 
-
+##### Hello World
 * chalice new-project hellochalice
 * chalice deploy
 * http https://19bdrm9ink.execute-api.ap-southeast-2.amazonaws.com/api/
+
+##### Hello Seattle
+* http https://19bdrm9ink.execute-api.ap-southeast-2.amazonaws.com/api/cities/seattle
+
+
 
--- app.py
+++ app.py
@@ -2,10 +2,19 @@ from chalice import Chalice
 
 app = Chalice(app_name='hellochalice')
 
+CITIES_TO_STATE = {
+    'seattle': 'WA',
+    'portland': 'OR',
+}
+
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+@app.route('/cities/{city}')
+def state_of_city(city):
+    return {'state': CITIES_TO_STATE[city]}
 
 
 # The view function above will return {"hello": "world"}
